dab89390e8ec12acbb8284c2b1a8712e73c7468b
Amashing0411 <mjalcantara0411@gmail.com>
2026-09-24 16:32:35 +0800

    chore: initialize Firebase backend local environment

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,7 @@
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+
+// admin.initializeApp();
+
+// Endpoint definitions will go here.
+// Currently empty for initialization purposes.
